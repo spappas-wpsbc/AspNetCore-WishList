2b7852eed0d7c70f7d973ff3246ab57369540014
Stephen <pappass@wpsbc.org>
2020-10-26 11:37:02 -0400

    second try

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,12 +10,12 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Error()
         {
-            return View("An error has occurred. Please try again.");
+            return View("Error");
         }
     }
 }
