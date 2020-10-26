68c6cf7f34d6cf86e65840bd6c229ffd7656ed55
Stephen <pappass@wpsbc.org>
2020-10-26 11:23:30 -0400

    undo remove

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -27,6 +27,7 @@ namespace WishList
             else app.UseExceptionHandler("/Home/Error");
             
             app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
